151cbff5ebae54b2676c389386b0b8ae2edcba4a
Ned Watson <watsonle@iastate.edu>
2022-08-16 00:43:32 -0500

    Add server-side script for parsing config file

--- server/server-deploy.sh
+++ server/server-deploy.sh
@@ -1,11 +1,20 @@
 # spacewar-dev,~/dev.moschata.net/spacewar,github.com/thelearned1/spacewar,dev,
-# \std.sh
-# spacewar,public_html/spacewar,github.com/thelearned1/spacewar,main,std.sh
+# spacewar,public_html/spacewar,github.com/thelearned1/spacewar,main,
 while IFS=',' read -r name loc repo branch script;
 do 
   if [[ $1 = $name ]]; then 
     echo "Deploying $repo into $loc from branch $branch"
-    exit
+    if [[ -z $script ]]; then 
+      if [[ -d $loc ]]; then 
+        cd $loc 
+        git pull origin $branch
+      else 
+        git clone $repo $loc
+      fi
+    else 
+      $($script $name $loc $repo $branch)
+    fi 
+    exit 0
   fi
 done < deploy.cfg
 echo "I don't recognize a project called '$1'" 1>&2
